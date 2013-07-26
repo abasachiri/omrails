fa91902ad026da3b100981562c206b194158b575
abasa <abasap@gmail.com>
2013-07-26 19:48:22 +0900

    Temporarily remove :recoverable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
+  # :token_authenticatable, :confirmable, :recoverable
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
   attr_accessible :email, :password, :password_confirmation, :name, :remember_me, :provider, :uid, :as => [:default, :admin]
 end
