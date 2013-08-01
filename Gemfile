1533f36e556f41b158b8902b4d061bef7ddde788
abasa <abasap@gmail.com>
2013-08-01 22:36:39 +0900

    Add jQuery Masonry

--- Gemfile
+++ Gemfile
@@ -4,7 +4,13 @@ gem 'rails', '3.2.12'
 gem 'jquery-rails'
 gem 'devise'
 gem 'simple_form'
-gem "paperclip", "~> 3.0"
+#gem "paperclip", "~> 3.0"
+#gem "paperclip", "~> 2.7"
+gem "paperclip", "~> 3.4.2"
+
+#gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
+gem "bcrypt-ruby"
+#gem "cocaine", "0.3.2"
 
 group :production do
 	gem 'pg'
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,10 +3,13 @@ class Pin < ActiveRecord::Base
 
   validates :description, presence: true
   validates :user_id, presence: true
-  validates_attachment :image, presence: true,
-  							   content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-  							   size: { less_than: 5.megabytes }
-
+  #validate :attachment, :image, presence: true,
+  #validates_attachment :image, presence: true,
+  #							   content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+  #							   size: { less_than: 5.megabytes }
+validates_attachment_presence :image
+validates_attachment_size :image, :less_than => 5.megabytes
+validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/png', 'image/gif']
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240"}
 end
